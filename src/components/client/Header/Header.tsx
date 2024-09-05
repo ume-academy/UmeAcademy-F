c1cb6f28444329de1d0dbb2edc973c68192a7408
phmvu2912 <phmvu2912@gmail.com>
2024-09-06 02:10:54 +0700

    feat[banner-cient]: SCRUM-67 Tạo banner trang chủ cho phía client

---
+++ src/components/client/Header/Header.tsx
@@ -0,0 +1,133 @@
+import { BookOutlined, HistoryOutlined, LogoutOutlined, QuestionCircleOutlined, SearchOutlined, UserOutlined } from '@ant-design/icons'
+import { Avatar, Button, Divider, Dropdown, Menu } from 'antd'
+import { Link } from 'react-router-dom'
+import { navigationCategory } from '../../../contants/client'
+import styles from './header.module.scss'
+
+interface Props {
+    
+}
+
+const Header = (props: Props) => {
+    
+    const menuItems: any = [
+        {
+          key: 'profile',
+          label: (
+            <div className="flex items-center">
+              <Link to={`/admin`}><Avatar icon={<UserOutlined />} /></Link>
+              <div className="ml-3 ">
+                <div className="flex mb-1">
+                <Link className='inline font-semibold text-[16px] max-w-[180px] truncate' to={`/admin`}>Tuấn Thái</Link>
+                <div className={`${styles['border_teacher']} text-[10px] flex items-center justify-center ml-3`}>
+                  <span className={`${styles['backGR_teacher']} block font-bold rounded-[5px] px-1 py-0.5 text-[#0058FA] hover:text-[#0058FA]`}>Teacher</span>
+                </div>     
+              </div>
+                <Link to={`/admin`} className='block text-[#999999] max-w-[180px] truncate hover:text-[#999999]'>thaiquoctuan1308@gmail.com</Link>
+              </div>
+            </div>
+          ),
+        },
+        {
+          type: 'divider'
+        },
+        {
+          key: 'courses',
+          label: (
+            <div className="flex items-center">
+              <BookOutlined />
+              <span className="ml-2">Bài giảng của tôi</span>
+            </div>
+          ),
+        },
+        {
+          key: 'myCourses',
+          label: (
+            <div className="flex items-center">
+              <QuestionCircleOutlined />
+              <span className="ml-2">Khóa học của tôi</span>
+            </div>
+          ),
+        },
+        {
+          key: 'history',
+          label: (
+            <div className="flex items-center">
+              <HistoryOutlined />
+              <span className="ml-2">Lịch sử thanh toán</span>
+            </div>
+          ),
+        },
+        {
+          type: 'divider',
+        },
+        {
+          key: 'logout',
+          label: (
+            <div className="flex items-center">
+              <LogoutOutlined />
+              <span className="ml-2">Sign Out</span>
+            </div>
+          ),
+        },
+      ];
+
+    return (
+        <>
+          <header>
+            {/* navTop */}
+            <div className="flex justify-between items-center mx-[40px]">
+                <div className="flex items-center min-w-[706px] justify-between">
+                    <div className="w-[84px] h-[70px] mr-[20px] bg-[#D9D9D9] flex items-center justify-center">
+                        <p className='text-black font-bold'>Logo</p>
+                    </div>
+                    <div className="flex items-center border-[1px]  rounded-full h-[46px] w-[498px] p-[12px]">
+                        <SearchOutlined style={{fontSize: 30, paddingRight: 12, color:'#C5C5C5'}} />
+                         <input type="text" className='w-full h-[26px] focus:outline-none' placeholder='Lessons name'/>
+                    </div>
+                </div>
+
+                {/* khi có data đoạn này dùng để if kiểm tra là user hay guest*/}
+                <div className="flex items-center min-w-[706px] justify-end font-semibold">
+
+                   {/* <===== Start teacher =====>*/}
+                   <Link className='mr-[60px] hover:text-[#FB9C46]' to={``}>Khóa học của tôi</Link>
+                    <Dropdown overlay={<Menu items={menuItems} />} trigger={['hover']} placement='bottomLeft'>
+                        <Button className="flex items-center justify-start border-[1px] border-[#C5C5C5] p-2 h-[46px] rounded-[14px] min-w-[180px]">
+                            <Avatar size="small" icon={<UserOutlined />} />
+                            <span className="ml-1 font-semibold max-w-[120px] truncate">Tuấn Thái</span>
+                            <div className={`${styles['border_teacher']} text-[10px] flex items-center justify-center`}>
+                              <span className={`${styles['backGR_teacher']} block font-bold rounded-[5px] px-1 py-0.5 bg-[rgba(234, 241, 255, 1)]  text-[#0058FA] hover:text-[#0058FA]`}>Teacher</span>
+                            </div>                            
+                        </Button>
+                    </Dropdown>
+                </div>
+            </div> 
+
+            {/* Nav bottom */}
+            <div className="flex bg-[#AC5E07] text-white justify-around items-center">
+                {navigationCategory.map((title) => (
+                <ul className='h-[60px] flex items-center' key={title.id}>
+                    <li className=''><Link to={`#`} className='text-[20px] font-semibold hover:underline p-4'>{title.title}</Link></li>
+                </ul>    
+                ))}
+            </div> 
+          </header>  
+        </>
+    )
+}
+
+export default Header
+
+                            {/* <div className="block">
+                                <p className='block'>Thái Tuấn</p>
+                                <p className='block'>nguyentrungduc2807@gmail.com</p>
+
+                            </div> */}
+
+
+                    {/* <===== Start khách =====>*/}
+                    {/* <Link className='mr-[60px] hover:text-[#FB9C46]' to={``}>Giảng dạy trên UmeAcademy</Link>
+                    <Link className='mr-[60px] bg-[#407F55] py-1.5 px-4 rounded-[8px] text-white hover:opacity-95' to={``}>Login</Link>
+                    <Link className='bg-[#FB9C46] py-1.5 px-4 rounded-[8px] text-white hover:opacity-95' to={``}>Sign Up</Link> */}
+                    {/* End khách */}
---
+++ src/contants/client.ts
@@ -0,0 +1,29 @@
+{/* <===== Đây là nơi lưu trữ những hằng phía client =====>*/ }
+
+// ** Image banner
+export const bannerImg = {
+    image: '../../../assets/images/client/HomePage/banner.jpg'
+}
+
+// ** Footer
+//! Màu background
+const backgroundFooterColor = 'bg-[#AC5E07]';
+
+// Logo Header
+export const logoClient = 'https://www.wallpaperflare.com/static/765/775/653/errors-minimalism-typography-x-wallpaper.jpg'     
+
+// danh mục ở header
+export const navigationCategory = [
+    {id: 1,title: "Khoa học"},
+    {id: 2,title: "Tin học"},
+    {id: 3,title: "Phát triển"},
+    {id: 4,title: "Tài chính & Kế toán"},
+    {id: 5,title: "Kinh doanh"}
+]
+
+// ** Footer
+// ! Màu background
+export const backgroundFooterColor = 'bg-[#AC5E07]';
+
+
+
