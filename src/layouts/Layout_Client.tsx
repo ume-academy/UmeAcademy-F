6b89c77e43ee60d91a7166e68370315891bf1397
phmvu2912 <phmvu2912@gmail.com>
2024-09-06 20:53:19 +0700

    feat[search-page-client]: SCRUM-54 Thiết kế giao diện tìm kiếm phía Client

--- src/layouts/Layout_Client.tsx
+++ src/layouts/Layout_Client.tsx
@@ -1,6 +1,7 @@
 import { Outlet } from 'react-router-dom'
-import Footer from '../components/client/Footer/Footer'
+// import Footer from '../components/client/Footer/Footer'
 import Header from '../components/client/Header/Header'
+import Footer from '../components/client/footer/Footer'
 
 interface Props {
     
