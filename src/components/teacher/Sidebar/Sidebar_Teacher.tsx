8a534c556f149faf1692c49496cb61d8649df3a9
Pham Dao Vu <145467039+phmvu2912@users.noreply.github.com>
2024-09-10 14:41:42 +0700

    refactor[sidebar-teacher]: SCRUM-111 Cập nhật lại đường dẫn component và tên  folder footer -> Footer (#23)
    * feat[sidebar-teacher]: SCRUM-111 Thiết kế sidebar cho role teacher
    
    * refactor[sidebar-teacher]: SCRUM-111 Cập nhật lại đường dẫn component và tên folder footer -> Footer

---
+++ src/components/teacher/Sidebar/Sidebar_Teacher.tsx
@@ -0,0 +1,51 @@
+import { DesktopOutlined, FileOutlined, PieChartOutlined, TeamOutlined, UserOutlined } from '@ant-design/icons';
+import { Menu, MenuProps } from 'antd';
+import Sider from 'antd/es/layout/Sider';
+import React, { useState } from 'react';
+import { NavLink } from 'react-router-dom';
+
+type MenuItem = Required<MenuProps>['items'][number];
+
+function getItem(
+  label: React.ReactNode,
+  key: React.Key,
+  icon?: React.ReactNode,
+  children?: MenuItem[],
+): MenuItem {
+  return {
+    key,
+    icon,
+    children,
+    label,
+  } as MenuItem;
+}
+
+const items: MenuItem[] = [
+  getItem(<NavLink to={'/teacher'}>Dashboard</NavLink>, '1', <PieChartOutlined />),
+  getItem(<NavLink to={'/teacher/courses'}>Bài giảng của tôi</NavLink>, '2', <DesktopOutlined />),
+  getItem('User', 'sub1', <UserOutlined />, [
+    getItem('Tom', '3'),
+    getItem('Bill', '4'),
+    getItem('Alex', '5'),
+  ]),
+  getItem('Team', 'sub2', <TeamOutlined />, [getItem('Team 1', '6'), getItem('Team 2', '8')]),
+  getItem('Files', '9', <FileOutlined />),
+];
+
+const Sidebar = () => {
+  const [collapsed, setCollapsed] = useState(false);
+
+  return (
+    <Sider 
+      collapsible 
+      collapsed={collapsed} 
+      onCollapse={(value) => setCollapsed(value)}
+      theme='light'
+    >
+      <div className="demo-logo-vertical" />
+      <Menu theme="light" defaultSelectedKeys={['1']} mode="inline" items={items} />
+    </Sider>
+  );
+}
+
+export default Sidebar
