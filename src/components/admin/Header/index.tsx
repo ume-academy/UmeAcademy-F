bbf756bd668813cb06c98e6f679e9a764f4ddd7a
ducnt28 <nguyentrungduc2807@gmail.com>
2024-09-12 15:54:55 +0700

    feat[history-transaction-admin]: SCRUM-123 Cập nhật lại css

--- src/components/admin/Header/index.tsx
+++ src/components/admin/Header/index.tsx
@@ -1,5 +1,4 @@
 import DropdownUser from './DropdownUser';
-import DarkModeSwitcher from './DarkModeSwitcher';
 
 const Header = (props: {
   sidebarOpen: string | boolean | undefined;
@@ -9,11 +8,6 @@ const Header = (props: {
     <header className="sticky top-0 z-999 flex  w-full bg-white drop-shadow-1 dark:bg-boxdark dark:drop-shadow-none">
       <div className="flex flex-grow items-center justify-end px-4 py-4 shadow-2 md:px-6 2xl:px-11">
         <div className="flex items-center gap-3 2xsm:gap-7">
-          <ul className="flex items-center gap-2 2xsm:gap-4">
-            {/* <!-- Dark Mode Toggler --> */}
-            <DarkModeSwitcher />
-            {/* <!-- Dark Mode Toggler --> */}
-          </ul>
 
           {/* <!-- User Area --> */}
           <DropdownUser />
